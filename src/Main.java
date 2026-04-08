622031e31f17ccbcfa464fe05d099a7eecc1f883
robert <robertgabriel.tatu@ulbsibiu.ro>
2026-04-08 04:28:26 +0300

    Tema casa laborator 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,20 +18,23 @@ public class Main {
             System.out.println(student);
         }
 
+        //schimbare lista in HashSet pentru cautare cu 0(1)
+        Set<Student> studentiSet = new HashSet<>(studenti);
+
         //b) verificare Alis Popa TI 21/2
         System.out.println("VERIFICARE Alis Popa");
         Student studentCautat1= new Student(120, "Alis", "Popa", "TI21/2");
-        boolean existaAlis = existaStudentInLista(studenti, studentCautat1);
+        boolean existaAlis = existaStudentInHashSet(studentiSet, studentCautat1);
         System.out.println("Studentul Alis Popa exista in lista: " + existaAlis);
 
         //c) verificare Maria Popa TI 21/1
         System.out.println("VERIFICARE Maria Popa");
         Student studentCautat2= new Student(112, "Maria", "Popa", "TI21/1");
-        boolean existaMaria = existaStudentInLista(studenti, studentCautat2);
+        boolean existaMaria = existaStudentInHashSet(studentiSet, studentCautat2);
         System.out.println("Studenta Maria Popa exista in lista: " + existaMaria);
     }
 
-    public static boolean existaStudentInLista(List<Student> studenti, Student student) {
+    public static boolean existaStudentInHashSet(Set<Student> studenti, Student student) {
         return studenti.contains(student);
     }
 }
